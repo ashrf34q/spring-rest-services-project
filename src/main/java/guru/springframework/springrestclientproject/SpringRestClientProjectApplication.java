f7f25d40b2f5e14a90f3b2ecba61e9338e4c3310
Ashraf Makki <makkiashraf08@gmail.com>
2022-08-27 13:25:07 -0400

    Initial commit

---
+++ src/main/java/guru/springframework/springrestclientproject/SpringRestClientProjectApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.springrestclientproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestClientProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestClientProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/springrestclientproject/SpringRestClientProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.springrestclientproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestClientProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
